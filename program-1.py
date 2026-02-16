0ae38827b347c469ff6aa0a1de4647081335c925
Riya Raj <lorven.riya.feb2007@gmail.com>
2026-02-16 20:12:37 +0530

    Revision of python-1

---
+++ program-1.py
@@ -0,0 +1,22 @@
+a=98
+b=100
+print(a+b)
+print(b**0.5)
+print(a**0.5)
+print(0.5*b*a)
+temp=0
+temp=a
+a=b
+b=temp
+print(a," ",b)
+print("km to mile",a*0.621371)
+if(a>0):
+    print("The number is POSITIVE")
+elif(a==0):
+    print("The number is ZERO")
+else:
+    print("The number is NEGATIVE")
+if(b%2==0):
+    print("EVEN")
+else:
+    print("ODD")
