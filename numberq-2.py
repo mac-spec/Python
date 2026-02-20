8071ad7f7fd979320818d1238964cc33667abbb3
Riya Raj <lorven.riya.feb2007@gmail.com>
2026-02-20 21:02:50 +0530

    Continued number code

--- numberq-2.py
+++ numberq-2.py
@@ -1,7 +1,14 @@
-# convert temperature celsius to fahrenheit
+"""# convert temperature celsius to fahrenheit
 c=int(input("Enter temperature in celsius"))
 f=(c*(9/5))+32
-print("The converted value is ",f," fahrenheit")
+print("The converted value is ",f," fahrenheit")"""
 
 
-#
+#Fibonnaci series
+n = int(input("Enter a number: "))
+a, b = 0, 1
+
+print("Fibonacci Series:")
+for _ in range(n):
+    print(a)
+    a, b = b, a + b
